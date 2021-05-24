3f877a1526c5b9239b299b48a246fcca7249e069
USER <ryansutawijaya@student.ub.ac.id>
2021-05-24 15:56:07 +0700

    splash screen & logout

--- app/src/main/java/com/example/papb_pa/Logout.kt
+++ app/src/main/java/com/example/papb_pa/Logout.kt
@@ -1,51 +1,36 @@
 package com.example.papb_pa
 
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.Gravity
-import android.widget.Toast
-import com.example.papb_pa.data.User
-import com.example.papb_pa.waitingroom.HeroAdapter
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_logout.*
-import kotlinx.android.synthetic.main.fragment_jawab.*
-import kotlinx.android.synthetic.main.fragment_jawab.view.*
-import java.util.*
 import kotlin.system.exitProcess
 
 class Logout : AppCompatActivity() {
-    private val database = FirebaseDatabase.getInstance("https://jedekan-gambar-default-rtdb.asia-southeast1.firebasedatabase.app/")
-    lateinit var id : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logout)
-        btn_iyo.setOnClickListener{
+        btn_iyo.setOnClickListener {
             finishAffinity()
             exitProcess(0)
         }
+            btn_nggak.setOnClickListener {
+                finish()
+            }
 
-        btn_nggak.setOnClickListener{
-            finish()
-        }
+            val dm = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(dm)
 
-        val dm = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(dm)
+            val lebar = dm.widthPixels
+            val tinggi = dm.heightPixels
 
-        val lebar = dm.widthPixels
-        val tinggi = dm.heightPixels
+            window.setLayout(((lebar * .6).toInt()), ((tinggi * .4).toInt()))
 
-        window.setLayout(((lebar*.6).toInt()), ((tinggi*.4).toInt()))
-
-        val params = window.attributes
-        params.gravity = Gravity.CENTER
-        params.x = 0
-        params.y = -20
-        window.attributes = params
+            val params = window.attributes
+            params.gravity = Gravity.CENTER
+            params.x = 0
+            params.y = -20
+            window.attributes = params
     }
-
 }
---
+++ app/src/main/java/com/example/papb_pa/Splash.kt
@@ -0,0 +1,40 @@
+package com.example.papb_pa
+
+import android.content.Intent
+import android.provider.SyncStateContract
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBar
+import com.daimajia.androidanimations.library.Techniques
+import com.viksaa.sssplash.lib.activity.AwesomeSplash
+import com.viksaa.sssplash.lib.cnst.Flags
+import com.viksaa.sssplash.lib.model.ConfigSplash
+
+
+class Splash : AwesomeSplash() {
+    override fun initSplash(configSplash: ConfigSplash?) {
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //menambahkan background
+        configSplash!!.backgroundColor = R.color.kuning
+        configSplash.animCircularRevealDuration = 3000
+        configSplash.revealFlagX = Flags.REVEAL_LEFT
+        configSplash.revealFlagX = Flags.REVEAL_BOTTOM
+
+        configSplash.logoSplash = R.drawable.gambarmenu
+        configSplash.animLogoSplashDuration = 2000
+        configSplash.animTitleTechnique = Techniques.FadeIn
+
+        configSplash.titleSplash = "Jedekan Gambar"
+        configSplash.titleTextColor=R.color.white
+        configSplash.titleTextSize = 50f
+        configSplash.animLogoSplashDuration = 2000
+        configSplash.animTitleTechnique = Techniques.FlipInX
+        configSplash.titleFont = "fonts/goodunicornregularrxev.ttf"
+    }
+    override fun animationsFinished() {
+        startActivity(Intent(this@Splash, MainActivity::class.java))
+    }
+
+}
